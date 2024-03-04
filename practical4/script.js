877d5515fc15a00435a945f6d2294f429cde4eb5
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-03-05 00:12:34 +0200

    Update script.js

--- practical4/script.js
+++ practical4/script.js
@@ -1,1 +1,46 @@
+function MyMathPower(b, n) {
+    let r = 1;
+    for (let i = 0; i < n; i++) {
+        r *= b;
+    }
+    console.log(`The ${n}th power of ${b} equals ${r}`);
+}
+
+function MyMathPowerDefault1(b, n) {
+    MyMathPower(b, n === undefined ? 4 : n);
+}
+
+function MyMathPowerDefault2(b = 2, n = 4) {
+    MyMathPower(b, n);
+}
+
+function TestAllFunctions() {
+    console.log("Testing MyMathPowerDefault1 with both parameters provided:");
+    MyMathPowerDefault1(2, 4);
+
+    console.log("Testing MyMathPowerDefault1 with only base provided:");
+    MyMathPowerDefault1(4);
+
+    console.log("Testing MyMathPowerDefault1 with only exponent provided:");
+    MyMathPowerDefault1(undefined, 2);
+
+    console.log("Testing MyMathPowerDefault1 with neither parameter provided:");
+    MyMathPowerDefault1();
+
+    console.log("\n\n\n");
+
+    console.log("Testing MyMathPowerDefault2 with both parameters provided:");
+    MyMathPowerDefault2(2, 4);
+
+    console.log("Testing MyMathPowerDefault2 with only base provided:");
+    MyMathPowerDefault2(4);
+
+    console.log("Testing MyMathPowerDefault2 with only exponent provided:");
+    MyMathPowerDefault2(undefined, 2);
+
+    console.log("Testing MyMathPowerDefault2 with neither parameter provided:");
+    MyMathPowerDefault2();
+}
+
+TestAllFunctions();
 
