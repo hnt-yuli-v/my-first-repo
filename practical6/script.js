0c89aa4d9049ecd622e7464ab3a7df190b38bba7
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-03-14 22:04:06 +0200

    Update script.js

--- practical6/script.js
+++ practical6/script.js
@@ -1,1 +1,174 @@
+class Square {
+  constructor(a) {
+    this._a = a;
+  }
+
+  static help() {
+    console.log("Квадрат – це чотирикутник із чотирма рівними сторонами.");
+  }
+
+  get a() {
+    return this._a;
+  }
+
+  set a(value) {
+    this._a = value;
+  }
+
+  length() {
+    console.log("Периметр квадрата:", 4 * this._a);
+  }
+
+  square() {
+    console.log("Площа квадрата:", this._a * this._a);
+  }
+
+  info() {
+    console.log("Інформація про квадрат:");
+    console.log("Довжина сторони a :", this._a);
+    console.log("Периметр:", 4 * this._a);
+    console.log("Площа:", this._a * this._a);
+  }
+}
+
+class Rectangle extends Square {
+  constructor(a, b) {
+    super(a);
+    this._b = b;
+  }
+
+  static help() {
+    console.log("Прямокутник – це чотирикутник, у якого протилежні сторони паралельні та рівні між собою.");
+  }
+
+  get b() {
+    return this._b;
+  }
+
+  set b(value) {
+    this._b = value;
+  }
+
+  length() {
+    console.log("Периметр прямокутника:", 2 * (this._a + this._b));
+  }
+
+  square() {
+    console.log("Площа прямокутника:", this._a * this._b);
+  }
+
+  info() {
+    console.log("Інформація про прямокутник:");
+    console.log("Довжина:", this._a);
+    console.log("Ширина:", this._b);
+    console.log("Периметр:", 2 * (this._a + this._b));
+    console.log("Площа:", this._a * this._b);
+  }
+}
+
+class Rhombus extends Square {
+  constructor(a, alpha, beta) {
+    super(a);
+    this._alpha = alpha;
+    this._beta = beta;
+  }
+
+  static help() {
+    console.log("Ромб — це паралелограм, який має рівні сторони..");
+  }
+
+  get alpha() {
+    return this._alpha;
+  }
+
+  set alpha(value) {
+    this._alpha = value;
+  }
+
+  get beta() {
+    return this._beta;
+  }
+
+  set beta(value) {
+    this._beta = value;
+  }
+
+  length() {
+    console.log("Периметр ромба:", 4 * this._a);
+  }
+
+  square() {
+    console.log("Площа ромба:", (this._a * this._a * Math.sin(this._alpha * Math.PI / 180)).toFixed(2));
+  }
+
+  info() {
+    console.log("Інформація про ромб:");
+    console.log("Довжина сторони:", this._a);
+    console.log("Тупий кут  у градусах:", this._alpha);
+    console.log("Гострий кут  у градусах:", this._beta);
+    console.log("Периметр:", 4 * this._a);
+    console.log("Площа:", (this._a * this._a * Math.sin(this._alpha * Math.PI / 180)).toFixed(2));
+  }
+}
+
+class Parallelogram extends Rectangle {
+  constructor(a, b, alpha, beta) {
+    super(a, b);
+    this._alpha = alpha;
+    this._beta = beta;
+  }
+
+  static help() {
+    console.log("Паралелограм - це чотирикутник з двома парами паралельних сторін.");
+  }
+
+  get alpha() {
+    return this._alpha;
+  }
+
+  set alpha(value) {
+    this._alpha = value;
+  }
+
+  get beta() {
+    return this._beta;
+  }
+
+  set beta(value) {
+    this._beta = value;
+  }
+
+  length() {
+    console.log("Периметр паралелограма:", 2 * (this._a + this._b));
+  }
+
+  square() {
+    console.log("Площа паралелограма:", this._a * this._b * Math.sin(this._alpha * Math.PI / 180));
+  }
+
+  info() {
+    console.log("Інформація про паралелограм:");
+    console.log("Довжина:", this._a);
+    console.log("Ширина:", this._b);
+    console.log("Тупий кут  у градусах:", this._alpha);
+    console.log("Гострий кут  у градусах:", this._beta);
+    console.log("Периметр:", 2 * (this._a + this._b));
+    console.log("Площа:", this._a * this._b * Math.sin(this._alpha * Math.PI / 180));
+  }
+}
+
+Square.help();
+Rectangle.help();
+Rhombus.help();
+Parallelogram.help();
+
+const square = new Square(7);
+const rectangle = new Rectangle(5, 9);
+const rhombus = new Rhombus(5, 15, 90);
+const parallelogram = new Parallelogram(3, 8, 65, 130);
+
+square.info();
+rectangle.info();
+rhombus.info();
+parallelogram.info();
 
