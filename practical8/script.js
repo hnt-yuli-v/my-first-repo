95098c887b4c7f6a0d46bc2ccee47864b5e20078
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-04-02 09:56:23 +0300

    Update script.js

--- practical8/script.js
+++ practical8/script.js
@@ -1,20 +1,17 @@
 import { speak as speakHello } from './library/SpeakHello.js';
-import { speak as speakGoodbye } from './library/SpeakGoodBye.js';
-import { speakAdd as speakAddGoodbye } from './library/SpeakGoodBye.js';
-(function() {
+import { speak as speakGoodbye, speakAdd as speakAddGoodbye } from './library/SpeakGoodBye.js';
+
 var names = ["Bill", "John", "Jen", "Jason", "Paul", "Frank", "Steven", "Larry", "Paula", "Laura", "Jim"];
 
-
 for (var i = 0; i < names.length; i++) {
-  if (names[i][0].toLowerCase() === 'j') {
-    console.log("Goodbye " + names[i]);
-  } else {
-    console.log("Hello " + names[i]);
-  }
+    if (names[i][0].toLowerCase() === 'j') {
+        speakGoodbye(names[i]);
+    } else {
+        speakHello(names[i]);
+    }
 }
 
 var additionalSelection = function(name) {
-    
     if (name.charAt(name.length - 1).toLowerCase() === 'a') {
         console.log(name + " ends with 'a'");
     }
@@ -23,4 +20,5 @@ var additionalSelection = function(name) {
 names.forEach(function(name) {
     additionalSelection(name);
 });
+
 
