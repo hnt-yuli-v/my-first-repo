abd471116e66d9848f31b0cadbc496b179f81c0f
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-04-09 12:34:57 +0300

    Update script.js

--- control2/script.js
+++ control2/script.js
@@ -1,1 +1,32 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function sortDescending(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] < arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+let sortedArrayDiv = document.getElementById('sortedArray');
+
+let array = [];
+for (let i = 0; i < 20; i++) {
+  array.push(getRandomInt(1, 100)); 
+}
+
+array = sortDescending(array);
+
+
+let sortedArrayText = document.createTextNode("Відсортований масив у спадаючому порядку: " + array.join(", "));
+
+
+sortedArrayDiv.appendChild(sortedArrayText);
 
