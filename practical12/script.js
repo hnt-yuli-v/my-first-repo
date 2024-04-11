3c0ade12e7789c76b4a8d30610b66fc9295d4544
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-04-11 23:28:51 +0300

    Update script.js

--- practical12/script.js
+++ practical12/script.js
@@ -1,1 +1,25 @@
+document.getElementById('submitBtn').addEventListener('click', function() {
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var meal = document.querySelector('input[name="meal"]:checked');
+    var dishes = document.querySelectorAll('input[name="dish"]:checked');
+    var comment = document.getElementById('comment').value;
+    var resultDiv = document.getElementById('result');
+
+    if (username === '' || email === '' || meal === null || dishes.length === 0 || comment === '') {
+        alert('Будь ласка, заповніть усі поля');
+        return;
+    }
+
+    var mealValue = meal.value;
+    var dishValues = [];
+    dishes.forEach(function(checkbox) {
+        dishValues.push(checkbox.value);
+    });
+
+    var resultText = `${username} (${email}) з'їв на ${mealValue} ${dishValues.join(', ')} та залишив такий коментар: "${comment}"`;
+    resultDiv.textContent = resultText;
+});
+
+
 
