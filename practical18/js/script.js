4e3103278b4756ffc873a6400ac14961fec6a85d
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-05-13 21:17:21 +0300

    Add files via upload

---
+++ practical18/js/script.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const burger = document.querySelector('.burger');
+
+    menuIcon.addEventListener('click', function() {
+        
+        const isActive = burger.classList.contains('active');
+
+        if (!isActive) {
+            burger.classList.add('active'); 
+        } else {
+            burger.classList.remove('active'); 
+        }
+    });
+});
+
+
+
+
+
+const slider = document.querySelector('.slider-wrapper');
+const prevButton = document.querySelector('.prev-btn');
+const nextButton = document.querySelector('.next-btn');
+let slideIndex = 0;
+
+function showSlides(index) {
+    const slides = document.querySelectorAll('.slide-card');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach((slide, i) => {
+        if (i === slideIndex) {
+            slide.style.display = "block";
+        } else {
+            slide.style.display = "none";
+        }
+    });
+}
+
+prevButton.addEventListener('click', () => {
+    slideIndex--;
+    showSlides(slideIndex);
+});
+
+nextButton.addEventListener('click', () => {
+    slideIndex++;
+    showSlides(slideIndex);
+});
+
+showSlides(slideIndex);
+
+setInterval(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 3000);
+
+
+
+
+
+
+
