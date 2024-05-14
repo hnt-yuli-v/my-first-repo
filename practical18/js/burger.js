d4ff225bb938a30b070656b92f2e2717927ffee2
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-05-14 17:48:09 +0300

    Update burger.js

--- practical18/js/burger.js
+++ practical18/js/burger.js
@@ -1,1 +1,8 @@
+function toggleMenu() {
+    var menuIcon = document.querySelector('.menu-icon');
+    menuIcon.classList.toggle('active');
 
+    var burgerMenu = document.querySelector('.burger');
+    burgerMenu.classList.toggle('active');
+}
+document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
