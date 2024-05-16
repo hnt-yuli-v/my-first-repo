7a5c86c97e3ab74be29eb0c4a22a1c99d10b81e9
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-05-16 13:19:49 +0300

    Update script.js

--- practical18/js/script.js
+++ practical18/js/script.js
@@ -1,16 +1,26 @@
 //BURGER
  
 
-const menuIcon = document.querySelector('.menu-icon');
-const burgerMenu = document.querySelector('.burger');
-
 function toggleMenu() {
-    menuIcon.classList.toggle('active'); // Додає або видаляє клас 'active' для кнопки бургера
-    burgerMenu.classList.toggle('active'); // Додає або видаляє клас 'active' для меню
+    var menuIcon = document.querySelector('.menu-icon');
+    menuIcon.classList.toggle('active');
+
+    var burgerMenu = document.querySelector('.burger');
+    burgerMenu.classList.toggle('active');
 }
 
+document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
 
-menuIcon.addEventListener('click', toggleMenu);
+document.addEventListener('click', function(event) {
+    var menuIcon = document.querySelector('.menu-icon');
+    var burgerMenu = document.querySelector('.burger');
+
+    if (!menuIcon.contains(event.target) && !burgerMenu.contains(event.target)) {
+        if (burgerMenu.classList.contains('active')) {
+            toggleMenu();
+        }
+    }
+ });
 
 
 // SLIDER 
