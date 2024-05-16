9647972956ffbb4c7c5df915c13cdd1387bef3f7
hnt-yuli-v <159455307+hnt-yuli-v@users.noreply.github.com>
2024-05-16 14:22:58 +0300

    Update script.js

--- practical18/js/script.js
+++ practical18/js/script.js
@@ -1,6 +1,5 @@
 //BURGER
  
-
 function toggleMenu() {
     var menuIcon = document.querySelector('.menu-icon');
     menuIcon.classList.toggle('active');
@@ -8,8 +7,19 @@ function toggleMenu() {
     var burgerMenu = document.querySelector('.burger');
     burgerMenu.classList.toggle('active');
 }
+
 document.querySelector('.menu-icon').addEventListener('click', toggleMenu);
 
+document.addEventListener('click', function(event) {
+    var menuIcon = document.querySelector('.menu-icon');
+    var burgerMenu = document.querySelector('.burger');
+
+    if (!menuIcon.contains(event.target) && !burgerMenu.contains(event.target)) {
+        if (burgerMenu.classList.contains('active')) {
+            toggleMenu();
+        }
+    }
+});
 
 
 // SLIDER 
